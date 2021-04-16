952e50f795dbf73f117d8ec47cc7c37297c60e4c
Subhaneel Agarwalla <a.subhaneel@gmail.com>
2021-04-16 14:44:56 +0530

    second changes

--- react_app_http/src/Containers/App.js
+++ react_app_http/src/Containers/App.js
@@ -1,17 +1,21 @@
 import './App.css';
 import Navbar from '../Components/Navbar'
 import Registration from '../Containers/Registration/Registration'
+import SimpleView from '../Containers/SimpleView';
+import { ThemeProvider } from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+      <div className="App">
+        <header className="App-header">
 
-      </header>
-    <Navbar/>
-<Registration/>
+        </header>
 
-    </div>
+        <Navbar />
+        <Registration />
+        {/* <SimpleView /> */}
+
+      </div>
   );
 }
 
--- react_app_http/src/Containers/Registration/Registration.js
+++ react_app_http/src/Containers/Registration/Registration.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
@@ -10,7 +9,7 @@ import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import axios from 'axios';
 import IndexView from '../IndexView/IndexView';
@@ -19,10 +18,14 @@ import Radio from '@material-ui/core/Radio';
 import FormLabel from '@material-ui/core/FormLabel';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControl from '@material-ui/core/FormControl';
+import validator from 'validator';
+import Validator from 'validator';
+import Form from 'react-validation/build/form';
+import Input from 'react-validation/build/input';
 
 function Copyright() {
   return (
-    <Typography variant="body1" color="textSecondary" align="center">
+    <Typography variant="overline" color="textSecondary" align="center" >
       {'Copyright © '}
       <Link color="inherit" href="http://localhost:3000/">
         Your Website
@@ -33,7 +36,7 @@ function Copyright() {
   );
 }
 
-const useStyles = theme => ({
+const useStyles = makeStyles(theme => ({
   paper: {
     marginTop: theme.spacing(8),
     display: 'flex',
@@ -51,151 +54,122 @@ const useStyles = theme => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
-
   cssOutlinedInput: {
     "&:hover $BorderOutline": {
       borderColor: "#3f51b5" //hovered
     },
   },
   BorderOutline: {},
-});
+}));
 
 
-class SignUp extends Component {
 
-  constructor() {
-    super();
+function SignUp() {
+  const classes = useStyles();
 
-    this.state = {
-      gender: ""
-    };
-    this.onChangeValue = this.onChangeValue.bind(this);
-  }
-
-
-  state = {
+  //   gender: '',
+  const [state = {
     firstName: "",
     lastName: '',
     emailId: '',
     gender: '',
     password: '',
-    TextField: "",
+  }] = useState([]);
 
-  }
-  // state = {
-  //   posts: []
-  // }
-
-  // componentDidMount() {
-  //   axios.get('http://localhost:62789/api/UserRegistration/Index')
-  //     .then(response => {
-  //       this.setState({ posts: response.data })
-  //       console.log(response);
-  //     });
-  // }
-
-
-  onChangeValue(event) {
-    console.log(event.target.value);
+  const onSuccess = (data, ev) => {
+    console.log(data);
   }
 
-  postDataHandler = () => {
+
+  const postDataHandler = () => {
     debugger;
+    if (state.firstName || state.password || state.emailId || state.lastName === "") {
+      // setError("Fields are required");
+      return;
+    }
     const updatePost = {
-      firstName: this.state.firstName,
-      lastName: this.state.lastName,
-      emailId: this.state.emailId,
-      gender: this.state.gender,
-      password: this.state.password,
+      firstName: state.firstName,
+      lastName: state.lastName,
+      emailId: state.emailId,
+      gender: state.gender,
+      password: state.password,
     };
     axios.post('http://localhost:62789/api/UserRegistration/GetSignupData', updatePost)
       .then(response => {
         debugger;
         console.log(response);
       });
-
   }
 
-  render() {
-    // const posts = this.state.posts.map(post => {
-    //   return <IndexView key={post.userId} data={post.emailId} />;
-    // });
-
-
-    const { classes } = this.props;
-    return (
-      <Container component="main" maxWidth="md" >
-
-        <CssBaseline />
+  return (
+    <Container component="main" maxWidth="md" >
+    <form>
         {/* {posts} */}
         <div className={classes.paper}>
           <Avatar className={classes.avatar}>
             <LockOutlinedIcon />
           </Avatar>
-          <Typography component="h1" variant="h5">
+          <Typography component="h1" variant="h5" style={{ paddingBottom: '15px' }}>
             Sign up
         </Typography>
-          <form className={classes.form} noValidate>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
 
-                {/* <CheckJs/> */}
+              {/* <CheckJs/> */}
 
-                <TextField
-                  autoComplete="fname"
-                  name="firstName"
-                  variant="outlined"
-                  required
-                  type="text"
-                  fullWidth
-                  label="First Name"
-                  value={this.state.firstName || ""}
-                  onChange={(event) => this.setState({ firstName: event.target.value })}
-                  InputProps={{
-                    classes: {
-                      root: classes.cssOutlinedInput,
-                      notchedOutline: classes.BorderOutline,
-                    },
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  variant="outlined"
-                  required
-                  fullWidth
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="lname"
-                  value={this.state.lastName || ""}
-                  onChange={(event) => this.setState({ lastName: event.target.value })}
-                  InputProps={{
-                    classes: {
-                      root: classes.cssOutlinedInput,
-                      notchedOutline: classes.BorderOutline,
-                    },
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  required
-                  fullWidth
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  value={this.state.emailId || ""}
-                  onChange={(event) => this.setState({ emailId: event.target.value })}
-                  InputProps={{
-                    classes: {
-                      root: classes.cssOutlinedInput,
-                      notchedOutline: classes.BorderOutline,
-                    },
-                  }}
-                />
-              </Grid>
-              <Grid item xs={12} style={{ paddingBottom: '20px' }}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                type="text"
+                fullWidth
+                label="First Name"
+                value={state.firstName}
+                InputProps={{
+                  classes: {
+                    root: classes.cssOutlinedInput,
+                    notchedOutline: classes.BorderOutline,
+                  },
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+                value={state.lastName}
+                InputProps={{
+                  classes: {
+                    root: classes.cssOutlinedInput,
+                    notchedOutline: classes.BorderOutline,
+                  },
+                }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                fullWidth
+                required
+                label="Email Address"
+                name="email"
+                type="email"
+                autoComplete="email"
+                value={state.emailId}
+                InputProps={{
+                  classes: {
+                    root: classes.cssOutlinedInput,
+                    notchedOutline: classes.BorderOutline,
+                  },
+                }}
+              />
+            </Grid>
+            {/* <Grid item xs={12} style={{ paddingBottom: '20px' }}>
                 <div>
 
                   <FormControl component="fieldset">
@@ -208,57 +182,55 @@ class SignUp extends Component {
                     </RadioGroup>
                   </FormControl>
                 </div>
-              </Grid>
-              <Grid item xs={12} style={{ paddingBottom: '20px' }}>
-                <TextField
-                  variant="outlined"
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  value={this.state.password || ""}
-                  onChange={(event) => this.setState({ password: event.target.value })}
-                  autoComplete="current-password"
-                  InputProps={{
-                    classes: {
-                      root: classes.cssOutlinedInput,
-                      notchedOutline: classes.BorderOutline,
-                    },
-                  }}
-                />
-              </Grid>
+              </Grid> */}
+            <Grid item xs={12} style={{ paddingBottom: '20px' }}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="Password"
+                value={state.password}
+                autoComplete="current-password"
+                InputProps={{
+                  classes: {
+                    root: classes.cssOutlinedInput,
+                    notchedOutline: classes.BorderOutline,
+                  },
+                }}
+              />
+            </Grid>
 
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant='contained'
-              color="primary"
-              style={{ height: '45px' }}
-              className={classes.submit}
-              onClick={this.postDataHandler}
-            >
-              Sign Up
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant='contained'
+            color="primary"
+            style={{ height: '45px' }}
+            className={classes.submit}
+            onClick={postDataHandler}
+          >
+            Sign Up
           </Button>
-            <Grid container justify="flex-end">
-              <Grid item>
-                <Link href="#" variant="body2">
-                  Already have an account? Sign in
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
               </Link>
-              </Grid>
             </Grid>
-          </form>
+          </Grid>
         </div>
-        <Box mt={5}>
+        <Box mt={5} style={{ marginTop: '30vh' }} >
           <Copyright />
         </Box>
-      </Container>
-    );
-  }
+      </form>
+    </Container >
+  );
 }
 
 
 // export default SignUp;
 
-export default withStyles(useStyles)(SignUp);
+export default SignUp;
